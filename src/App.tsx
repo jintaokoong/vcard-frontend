fa004827c7e154faee014ec1556de4dc6b7fd7f3
Jintao Koong <jintaokoong99@gmail.com>
2022-04-14 02:20:59 +0800

    ✨ firebase login

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,34 @@
+import { Box, Button, MantineProvider, PasswordInput, Space, TextInput } from '@mantine/core';
+import { useInputState } from '@mantine/hooks';
+import * as auth from 'firebase/auth';
+import { useEffect } from 'react';
+
 export default function App() {
-  return <div className="App">hello world</div>;
+  const [email, setEmail] = useInputState('');
+  const [password, setPassword] = useInputState('');
+  const authenticate = () => {
+    auth.signInWithEmailAndPassword(auth.getAuth(), email, password )
+    .then(() => console.log('logged in'));
+  }
+
+  useEffect(() => {
+    const sub = auth.onAuthStateChanged(auth.getAuth(), (user) => {
+      console.log(user);
+    }, (error) => {
+      console.error(error);
+    });
+    return () => {
+      sub();
+    }
+  }, [])
+
+
+  return <MantineProvider>
+    <Box>
+      <TextInput mb={'md'} label={'Email'} value={email} onChange={setEmail} />
+      <PasswordInput label={'Password'} value={password} onChange={setPassword} />
+      <Space h={'lg'} />
+      <Button fullWidth onClick={authenticate}>Submit</Button>
+    </Box>
+  </MantineProvider>;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import './configurations/firebase';
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
