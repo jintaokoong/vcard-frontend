8b47c6e5866294a3c5dfd2b9affaabb5556afc8b
Jintao Koong <jintaokoong99@gmail.com>
2022-04-14 16:32:23 +0800

    ✨ add user listing

--- src/hooks/use-users.ts
+++ src/hooks/use-users.ts
@@ -1,9 +1,17 @@
-import axiosInstance from "@/configurations/axios";
-import { useQuery } from "react-query";
+import axiosInstance from '@/configurations/axios';
+import { useQuery } from 'react-query';
+
+interface User {
+  uid: string;
+  email: string;
+  customClaims?: {
+    admin: boolean;
+  };
+}
 
 const useUsers = () => {
-  return useQuery(["users"], () =>
-    axiosInstance.get("/users").then(({ data }) => data)
+  return useQuery(['users'], () =>
+    axiosInstance.get<{ users: User[] }>('/users').then(({ data }) => data),
   );
 };
 
--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,7 +1,50 @@
-import { Text } from '@mantine/core';
+import { Button, Grid, Group, Menu, Paper, Text, Title } from '@mantine/core';
+import useUsers from '@/hooks/use-users';
+import { Fragment } from 'react';
+import { useLogger } from '@mantine/hooks';
 
 const Users = () => {
-  return <Text>Users</Text>;
+  const { data: { users } = { users: [] } } = useUsers();
+
+  useLogger('users', [users]);
+
+  return (
+    <Fragment>
+      <Group position={'apart'} align={'center'} mb={'sm'}>
+        <Title order={2}>Users</Title>
+        <Button size={'md'} variant={'light'}>
+          Invite
+        </Button>
+      </Group>
+      <Grid>
+        {users.map((u) => (
+          <Grid.Col key={u.uid} sm={12} md={3} lg={4}>
+            <Paper shadow={'sm'} p={'sm'}>
+              <Group
+                position={'apart'}
+                sx={{
+                  flexWrap: 'nowrap',
+                }}
+              >
+                <Text
+                  size={'md'}
+                  sx={{
+                    overflowX: 'hidden',
+                  }}
+                >
+                  {u.email}
+                </Text>
+                <Menu position={'bottom'} placement={'end'}>
+                  <Menu.Item>Delete</Menu.Item>
+                </Menu>
+              </Group>
+              <Text>{u.customClaims?.admin ? 'Admin' : 'User'}</Text>
+            </Paper>
+          </Grid.Col>
+        ))}
+      </Grid>
+    </Fragment>
+  );
 };
 
 export default Users;
